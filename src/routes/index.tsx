0aed82a71092f429347fd640fed7d80b6b57d4d9
zmt18928708528-eng <zmt18928708528@gmail.com>
2026-09-22 23:19:39 -0700

    feat: 看板门禁与登出，注册 login 路由

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,13 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { Board } from "@/components/kanban/board";
+import { LockGate } from "@/components/lock/lock-gate";
 
 export const Route = createFileRoute("/")({ component: Home });
 
 function Home() {
-  return <Board />;
+  return (
+    <LockGate>
+      <Board />
+    </LockGate>
+  );
 }
